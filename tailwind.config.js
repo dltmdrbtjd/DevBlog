2c6ef138587dae8fdc1c26ee3970f8ecdbb7bcbc
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-27 18:10:48 +0900

    setup typography default style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,38 @@
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: "media",
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'media',
   theme: {
     extend: {
       typography: (theme) => ({
         DEFAULT: {
           css: {
             h1: {
-              color: '#FF5C58',
+              color: 'rgb(203 213 225)',
+            },
+            h2: {
+              color: 'rgb(203 213 225)',
+            },
+            h3: {
+              color: 'rgb(203 213 225)',
+            },
+            p: {
+              color: 'rgb(203 213 225)',
+            },
+            li: {
+              color: 'rgb(203 213 225)',
+            },
+            strong: {
+              color: 'rgb(255 255 255)',
+            },
+            time: {
+              color: 'rgb(248 250 252)',
+              fontWeight: 'bold',
+            },
+            a: {
+              color: 'rgb(203 213 225)',
             },
           },
         },
