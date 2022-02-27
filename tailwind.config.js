51e3c54f336711bd4b27a8289767a2e2157269a5
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-27 16:59:39 +0900

    chore: purge -> content because tailwind 3.0 rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  content: [],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: "media",
   theme: {
     extend: {
