b7ccbba277ac60026471cf8d42a0dc08727315c8
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-28 00:46:48 +0900

    refac: Refactoring for server-side processing and separation of md file folders

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,58 +1,45 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import glob from 'glob'
+import memoizee from 'memoizee'
+import { Post } from '../types'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
-export function getSortedPostsData() {
-  const fileNames = fs.readdirSync(postsDirectory)
-  const allPostsData = fileNames.map((fileName) => {
-    const id = fileName.replace(/\.md$/, '')
+async function getSortedPostsData(): Promise<Array<Post>> {
+  const files = glob.sync(`${postsDirectory}/**/*.md`)
+  const posts = files
+    .reduce((prev, cur) => {
+      const file = fs.readFileSync(cur, { encoding: 'utf-8' })
+      const { data, content } = matter(file)
+      const { date, completed } = data
 
-    const fullPath = path.join(postsDirectory, fileName)
-    const fileContents = fs.readFileSync(fullPath, 'utf8')
+      const path = cur
+        .slice(cur.indexOf(postsDirectory) + postsDirectory.length + 1)
+        .replace('.md', '')
 
-    const matterResult = matter(fileContents)
-
-    return {
-      id,
-      ...(matterResult.data as {
-        date: string
-        title: string
-        subheading: string
-      }),
-    }
-  })
-
-  return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1
-    } else {
-      return -1
-    }
-  })
+      if (completed) {
+        const result = {
+          ...data,
+          content,
+          date,
+          path,
+        }
+        return [...prev, result]
+      }
+      console.log(content)
+      return prev
+    }, [])
+    .sort((a, b) => {
+      if (a.date < b.date) {
+        return 1
+      } else {
+        return -1
+      }
+    })
+  return posts
 }
 
-export function getAllPostIds() {
-  const fileNames = fs.readdirSync(postsDirectory)
-  return fileNames.map((fileName) => {
-    return {
-      params: {
-        id: fileName.replace(/\.md$/, ''),
-      },
-    }
-  })
-}
-
-export async function getPostData(id: string) {
-  const fullPath = path.join(postsDirectory, `${id}.md`)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
-
-  const { data, content } = matter(fileContents)
-
-  return {
-    id,
-    content,
-    ...(data as { date: string; title: string }),
-  }
-}
+export const getAllPosts: () => Promise<Array<Post>> =
+  memoizee(getSortedPostsData)
