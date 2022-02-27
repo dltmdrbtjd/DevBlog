11b24463a4dac8be98cadd655e9babf8a6edef3d
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-28 00:56:22 +0900

    Delete unused comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,6 @@ export default function Home({
 
 export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = await getAllPosts()
-  console.log(allPostsData[0].path)
   return {
     props: {
       allPostsData,
