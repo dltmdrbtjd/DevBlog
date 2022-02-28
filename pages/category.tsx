11355ecb610bcd7f329204d236494f788225d2aa
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-03-01 03:42:45 +0900

    create category page

--- pages/category.tsx
+++ pages/category.tsx
@@ -1,3 +1,23 @@
+import Layout from '../components/layout'
+import { getAllCategory } from '../lib/posts'
+import { GetStaticProps } from 'next'
+
 export default function Category() {
-  return 'category page!'
+  return (
+    <Layout>
+      <h2 className="text-5xl text-center border-b-[2px] border-b-gray-500 pb-5">
+        Category
+      </h2>
+      <div>tagslist</div>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const categories = await getAllCategory()
+  return {
+    props: {
+      categories,
+    },
+  }
 }
