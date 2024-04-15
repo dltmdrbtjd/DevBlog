fdf8e66f48ed4a3b237b6bd8174971056b47778f
dltmdrbtjd <dltmdrbtjd@gmail.com>
2024-04-16 00:04:21 +0900

    Fix: tailwind, postcss compile error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    // './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  darkMode: 'media',
   theme: {
-    extend: {
-      typography: () => ({
-        DEFAULT: {
-          css: {
-            h1: {
-              color: 'rgb(203 213 225)',
-            },
-            h2: {
-              color: 'rgb(203 213 225)',
-            },
-            h3: {
-              color: 'rgb(203 213 225)',
-            },
-            p: {
-              color: 'rgb(203 213 225)',
-              margin: 0,
-            },
-            li: {
-              color: 'rgb(203 213 225)',
-            },
-            strong: {
-              color: 'rgb(255 255 255)',
-            },
-            time: {
-              color: 'rgb(248 250 252)',
-              fontWeight: 'bold',
-            },
-            a: {
-              color: 'rgb(203 213 225)',
-            },
-          },
-        },
-      }),
-    },
+    extend: {},
+    screens: {},
   },
-  plugins: [require('@tailwindcss/typography')],
-}
+  plugins: [],
+};
