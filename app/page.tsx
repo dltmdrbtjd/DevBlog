86a15e63adb140ec959c0ca82945734e77aaa0e0
dltmdrbtjd <dltmdrbtjd@gmail.com>
2024-04-17 01:51:01 +0900

    Feat: Create main latest list page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import PostList from '@/components/post/PostList';
+import { DefaultNumberOfPosts } from '@/constant';
+import { getSortedPostsData } from '@/service/post';
+
+export default async function Home() {
+  const posts = await getSortedPostsData();
+  const slicesPosts = posts.slice(0, DefaultNumberOfPosts);
+
+  return <PostList posts={slicesPosts} title="Latest" />;
 }
