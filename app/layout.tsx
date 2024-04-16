2498250f6c6961c102f6ae263df9738c54a59975
dltmdrbtjd <dltmdrbtjd@gmail.com>
2024-04-17 01:50:49 +0900

    chore: remove unused tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,15 @@
-// ------------- Styles ----------------------
+// ** Styles **
 import '@/styles/global.css';
-//-------------- Components ------------------
-import Header from '@/components/Header';
+// ** Common Components **
+import Header from '@/components/common/Header';
+import GlobalLayout from '@/components/common/GlobalLayout';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
-        <div className="container mx-auto">
-          {/* <Head>
-        <link rel="icon" href="/favicon/favicon.ico" />
-        <meta name="description" content={desc} />
-        <meta property="og:image" content="/images/profile.jpeg" />
-        <meta name="og:title" content={title} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head> */}
-          <Header />
-          <main className="prose max-w-2xl mx-auto mt-28 px-4">{children}</main>
-          {/* {!back && (
-        <div className="max-w-2xl mx-auto text-white font-bold hover:text-emerald-200 px-4 mb-10 mt-10">
-          <button onClick={() => router.back()}>← Back to page</button>
-        </div>
-      )} */}
-        </div>
+        <Header />
+        <GlobalLayout>{children}</GlobalLayout>
       </body>
     </html>
   );
