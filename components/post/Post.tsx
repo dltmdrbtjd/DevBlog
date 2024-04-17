203a46df2813fbe6ab9e85f15f5d661850a10f4f
dltmdrbtjd <dltmdrbtjd@gmail.com>
2024-04-17 23:25:58 +0900

    Feat: Create New Post Detail Component

---
+++ components/post/Post.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import Link from 'next/link';
+import Markdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { useRouter } from 'next/navigation';
+import { Post } from '@/types';
+import Date from '@/components/post/Date';
+
+export default function PostLayout({ post }: { post: Post }) {
+  const router = useRouter();
+
+  return (
+    <article className="pb-20">
+      <div className="max-w-2xl mx-auto text-white font-bold hover:text-emerald-200 mb-10 mt-10">
+        <button onClick={() => router.back()}>← Back to page</button>
+      </div>
+      <h1 className="mb-2 text-[2rem] text-white font-bold">{post.title}</h1>
+      <div className="flex justify-between w-max mb-2">
+        {post?.category.map((t) => {
+          return (
+            <Link
+              href={`/category/${t}/pages/1`}
+              key={t}
+              className="cursor-pointer px-4 py-1 mr-2 mt-1 rounded-full bg-gray-400 font-semibold text-sm flex align-center w-max text-gray-800 no-underline"
+            >
+              {t}
+            </Link>
+          );
+        })}
+      </div>
+      <div className="text-white text-[1.25rem] my-2">
+        <Date dateString={post.date} />
+      </div>
+      <Markdown
+        className="text-white"
+        children={post.content}
+        components={{
+          h1: ({ node, ...props }) => (
+            <h1 className="text-[1.75rem] font-bold text-white mt-6" {...props} />
+          ),
+          h2: ({ node, ...props }) => (
+            <h2 className="text-[1.5rem] font-bold text-white mt-6" {...props} />
+          ),
+          h3: ({ node, ...props }) => (
+            <h3 className="text-[1.25rem] font-bold text-white mt-6" {...props} />
+          ),
+          ul: ({ node, ...props }) => <ul className="ml-3 list-disc" {...props} />,
+          li: ({ node, ...props }) => (
+            <li className="font-light leading-[1.75rem] ml-3" {...props} />
+          ),
+          ol: ({ node, ...props }) => <ol className="ml-3 list-decimal" {...props} />,
+          a: ({ node, ...props }) => (
+            <a className="text-cyan-100 hover:text-emerald-500" {...props} />
+          ),
+          code(props) {
+            const { children, className, node, ...rest } = props;
+            const match = /language-(\w+)/.exec(className || '');
+            return match ? (
+              <SyntaxHighlighter
+                {...rest}
+                PreTag="div"
+                children={String(children).replace(/\n$/, '')}
+                language={match[1]}
+                style={tomorrow}
+                ref={null} // Add a null ref to fix the type error
+              />
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
+    </article>
+  );
+}
